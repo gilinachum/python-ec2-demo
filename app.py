29999a26a7119b0dae19833b44273e6b60b5c4d2
EC2 Default User <ec2-user@ip-172-31-9-217.us-east-2.compute.internal>
2017-12-19 20:55:28 +0000

    reporting instanceId and subnetId

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import base64
 import json
+import urllib.request
 
 from flask import Flask, jsonify, abort, request
 from datetime import datetime
@@ -8,6 +9,9 @@ app = Flask(__name__)
 
 
 @app.route('/fetch')
-def fetch():
+def get():
+    instanceId = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
+    mac_id = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/network/interfaces/macs/').read().decode()
+    subnet_id = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/network/interfaces/macs/' + mac_id +'/subnet-id/').read().decode()
     print(f'Starting')    
-    return jsonify('{}')
+    return jsonify(instanceid=instanceId, subnet_id=subnet_id)
