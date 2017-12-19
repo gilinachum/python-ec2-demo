bc2e183a37d33396c86b10374ceadec465a0d8ea
EC2 Default User <ec2-user@ip-172-31-9-217.us-east-2.compute.internal>
2017-12-19 21:11:00 +0000

    Added busy-wait

--- app.py
+++ app.py
@@ -6,12 +6,22 @@ from flask import Flask, jsonify, abort, request
 from datetime import datetime
 
 app = Flask(__name__)
-
-
+    
 @app.route('/fetch')
 def get():
+    busy_wait_in_seconds  = request.args.get('busy_wait_in_seconds', default = 1, type = int)
+    
+    print(f'Starting') 
     instanceId = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
     mac_id = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/network/interfaces/macs/').read().decode()
     subnet_id = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/network/interfaces/macs/' + mac_id +'/subnet-id/').read().decode()
-    print(f'Starting')    
+    
+    busy_wait(busy_wait_in_seconds)
+    
     return jsonify(instanceid=instanceId, subnet_id=subnet_id)
+    
+def busy_wait(dt):   
+    import time
+    current_time = time.time()
+    while (time.time() < current_time+dt):
+        pass
---
+++ scratch.py
@@ -0,0 +1,17 @@
+import urllib.request
+
+def busy_wait(dt):   
+    import time
+    current_time = time.time()
+    while (time.time() < current_time+dt):
+        pass
+        
+
+
+instanceid = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
+mac_id = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/network/interfaces/macs/').read().decode()
+subnet_id = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/network/interfaces/macs/' + mac_id +'/subnet-id/').read().decode()
+busy_wait(5)
+
+
+print(instanceid)
