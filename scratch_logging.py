8387f655f863a4fc00da82e00d9c9ff6f579bf6b
EC2 Default User <ec2-user@ip-172-31-20-248.us-east-2.compute.internal>
2018-01-01 10:11:45 +0000

    changing log level debug->info

--- scratch_logging.py
+++ scratch_logging.py
@@ -1,3 +1,3 @@
 import logging
-logging.basicConfig(filename='./pythonapp.log',level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='./pythonapp.log',level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.info('So should this')
