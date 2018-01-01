bdad5d42055ce9e1447582e54b3c8cf38385fdc9
EC2 Default User <ec2-user@ip-172-31-20-248.us-east-2.compute.internal>
2018-01-01 09:33:59 +0000

    Adding logging: /home/ec2-user/pythonapp.log

--- app.py
+++ app.py
@@ -4,17 +4,24 @@ import urllib.request
 
 from flask import Flask, jsonify, abort, request
 from datetime import datetime
+import boto3
+import logging
+
+logging.basicConfig(filename='/home/ec2-user/pythonapp.log',level=logging.DEBUG)
+
+instanceId = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
+az = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone').read().decode()
+
+logging.info(f'Starting. instanceId={instanceId}. az={az}')
 
 app = Flask(__name__)
-    
+
 @app.route('/fetch')
 def get():
     busy_wait_in_seconds  = request.args.get('busy_wait_in_seconds', default = 1, type = int)
+    logging.info(f'Received request to wait {busy_wait_in_seconds} seconds.')
     
-    print(f'Starting') 
-    instanceId = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
-    az = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/placement/availability-zone').read().decode()
-    
+    record_metric(instanceId, busy_wait_in_seconds)
     busy_wait(busy_wait_in_seconds)
     
     return jsonify(instanceid=instanceId, availability_zone=az)
@@ -25,7 +32,27 @@ def busy_wait(dt):
     while (time.time() < current_time+dt):
         pass
     
-def send_hit_metric():
-    import boto3
+def record_metric(instance_id, busy_wait_in_seconds):
     client = boto3.client('cloudwatch')
-    
+    response = client.put_metric_data(
+    Namespace='python-app',
+    MetricData=[
+        {
+            'MetricName': 'calls',
+            'Dimensions': [
+                {
+                    'Name': 'busy_wait_in_seconds',
+                    'Value': str(busy_wait_in_seconds)
+                },
+                {
+                    'Name': 'instance_id',
+                    'Value': instance_id
+                }
+            ],
+            'Value': 1,
+            'Unit': 'None',
+            'StorageResolution': 1
+        },
+    ])
+    logging.info(f'recorded CW metric. Response={response}')
+    return response
